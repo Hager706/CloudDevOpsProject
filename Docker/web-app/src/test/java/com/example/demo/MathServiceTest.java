1fe48a6045ad13bd90fdcb7005ecfb0e23e2357e
Hager706 <hagertarek706@gmail.com>
2025-04-21 13:25:16 +0200

    remove public modifier from test class

--- Docker/web-app/src/test/java/com/example/demo/MathServiceTest.java
+++ Docker/web-app/src/test/java/com/example/demo/MathServiceTest.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-public class MathServiceTest {
+ class MathServiceTest {
 
     @Autowired
     private MathService mathService;
